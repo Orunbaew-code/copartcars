2bdd683cc27ed47154ff271f14e2fdf415861ec2
Orunbayew <orunbaew01@gmail.com>
2024-12-03 11:44:25 +0500

    Pipeline created and works great

--- copartscraper/pipelines.py
+++ copartscraper/pipelines.py
@@ -3,11 +3,32 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from datetime import datetime
+import pytz
 
 class CopartscraperPipeline:
     def process_item(self, item, spider):
+        # Remove "Icon" from "condition"
+        item['condition'] = item['condition'].replace(" Icon", "")
+        
+        # Replace "\n" with ", " in the address
+        item['address'] = item['address'].replace("\n", ", ")
+        
+        # Parse and convert "sale_date"
+        # Remove "UZT" from the string as it causes parsing issues
+        sale_date_str = item['sale_date'].replace(" UZT", "")
+        
+        # Parse the datetime without timezone
+        uzbekistan_time = datetime.strptime(sale_date_str, "Mon. %b %d, %Y %I:%M %p")
+        
+        # Set timezone to Asia/Tashkent
+        uz_tz = pytz.timezone('Asia/Tashkent')
+        localized_time = uz_tz.localize(uzbekistan_time)
+        
+        # Convert to UTC
+        utc_time = localized_time.astimezone(pytz.utc)
+        item['sale_date'] = utc_time.strftime("%Y-%m-%d %H:%M:%S %Z")
+        
         return item
--- copartscraper/settings.py
+++ copartscraper/settings.py
@@ -71,9 +71,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "copartscraper.pipelines.CopartscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "copartscraper.pipelines.CopartscraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
